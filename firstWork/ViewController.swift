924ec78c994bd2ff62ef1c5b12289db6f0fe6186
DogeCnx <thiraphat.re0096@gmail.com>
2021-01-19 21:34:06 +0700

    init project

---
+++ firstWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstWork
+//
+//  Created by APPMAN M-006 on 19/1/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
